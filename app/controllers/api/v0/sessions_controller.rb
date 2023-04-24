55804c7748b53e667ebe80a15983dbb8a09eafc3
Elle Majors <hellolovely@ellemajors.com>
2023-04-24 16:50:15 -0600

    test: user session

---
+++ app/controllers/api/v0/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V0::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      render json: UserSerializer.new(user), status: 200
+    else
+      render json: { error: "Invalid credentials" }, status: 400
+    end
+  end
+end
