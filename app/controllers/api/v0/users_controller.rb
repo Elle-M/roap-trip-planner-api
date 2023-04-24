be372fcc7d16208198dec53520d7413c5be61585
Elle Majors <hellolovely@ellemajors.com>
2023-04-24 16:04:58 -0600

    test: new user create

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -2,9 +2,9 @@ class Api::V0::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    # require 'pry'; binding.pry
+
     if user.save
-      render json: UsersSerializer.new(user), status: 201
+      render json: UserSerializer.new(user), status: 201
     else
       render json: { error: user.errors.full_messages.to_sentence }, status: 400
     end
--- spec/requests/api/v0/users_request_spec.rb
+++ spec/requests/api/v0/users_request_spec.rb
@@ -2,20 +2,32 @@ require "rails_helper"
 
 describe "User API" do
   it "can get a new user" do
-    user = User.create!(email: "bob@email.com", password: "password", password_confirmation: "password")
-    
     user_params= {
-        email: user.email,
-        password: user.password,
-        password_confirmation: user.password_confirmation
-      }.to_json
+        email: "bob@email.com", 
+        password: "password",
+        password_confirmation: "password"
+    }.to_json
 
     post "/api/v0/users", params: user_params, headers: { 'Content-Type': 'application/json' }
 
     parsed = JSON.parse(response.body, symbolize_names: true)
-    # require 'pry'; binding.pry
-    #returning :error=>"Email has already been taken"
+
     expect(response).to be_successful
-    expect(response.status).to eq(200)
+    expect(response.status).to eq(201)
   end  
+
+  it "can't get a new user with bad password confirmation" do
+    user_params= {
+      email: "bob@email.com", 
+      password: "password",
+      password_confirmation: ""
+    }.to_json
+
+    post "/api/v0/users", params: user_params, headers: { 'Content-Type': 'application/json' }    
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+  end 
 end
