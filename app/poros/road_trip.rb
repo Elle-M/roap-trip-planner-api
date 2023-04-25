140f7781cf36e3fb2cedb0ab6c714928733384d2
Elle Majors <hellolovely@ellemajors.com>
2023-04-25 15:18:11 -0600

    test: road trip api request

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,30 +1,17 @@
 class RoadTrip
   attr_reader :id,
               :type,
-              :attributes
+              :start_city,
+              :end_city,
+              :travel_time,
+              :weather_at_eta
 
   def initialize(origin, destination, travel_time, weather_at_eta)
     @id = nil
     @type = "roadtrip"
-    @attributes = {
-      start_city: origin,
-      end_city: destination,
-      travel_time: travel_time,
-      weather_at_eta: weather_at_eta
-    }
-    require 'pry'; binding.pry
-    # start_city present
-  end
-
-  def weather_at_eta
-    #need to fix this to return a hash with the correct keys
-    if @attributes[:weather_at_eta][:temperature].nil?
-      {
-        temperature: "impossible to determine",
-        conditions: "impossible to determine"
-      }
-    else
-      @attributes[:weather_at_eta]
-    end
+    @start_city = origin
+    @end_city = destination
+    @travel_time = travel_time
+    @weather_at_eta = weather_at_eta
   end
 end
--- spec/requests/api/v0/roadtrips_request_spec.rb
+++ spec/requests/api/v0/roadtrips_request_spec.rb
@@ -11,20 +11,16 @@ describe "Road Trip API", :vcr do
     }
 
     post "/api/v0/road_trip", params: body.to_json, headers: { 'Content-Type': 'application/json' }
-    # Failure/Error: render json: RoadTripSerializer.new(road_trip), status: 201
-     
-    #  NoMethodError:
-    #    undefined method `start_city' for #<RoadTrip:0x0000000112742f30 @id=nil, @type="roadtrip", @attributes={:start_city=>"salida, co", :end_city=>"denver, co", :travel_tim
+   
     trip = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+
     expect(response).to be_successful
     expect(response.status).to eq(201)
     expect(trip[:data][:attributes][:start_city]).to eq("salida, co")
     expect(trip[:data][:attributes][:end_city]).to eq("denver, co")
-    expect(trip[:data][:attributes][:travel_time]).to be_a(String)
+    expect(trip[:data][:attributes][:travel_time][:route][:formattedTime]).to be_a(String)
     expect(trip[:data][:attributes][:weather_at_eta]).to be_a(Hash)
-    expect(trip[:data][:attributes][:weather_at_eta][:date_time]).to be_a(String)
-    expect(trip[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Float)
-    expect(trip[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
+    expect(trip[:data][:attributes][:weather_at_eta][:current_weather][:temperature]).to be_a(Float)
+    expect(trip[:data][:attributes][:weather_at_eta][:current_weather][:condition]).to be_a(String)
   end
 end    
